7c180a2f94e7e1b48458a59483f9e7479ec08f56
Kxmm04 <Arpiwat0204@gmail.com>
2025-02-26 14:02:07 +0700

    update new

--- itApp/models.py
+++ itApp/models.py
@@ -10,6 +10,9 @@ class Category(models.Model):
         
 class Computer(models.Model):
     comid = models.CharField(max_length=13, default='', primary_key=True)
+    name = models.CharField(max_length=200, default='')
+    model = models.CharField(max_length=200, default='')
+    brand = models.CharField(max_length=200, default='')
     processor = models.CharField(max_length=200, default='')
     graphic = models.CharField(max_length=200, default='')
     display = models.CharField(max_length=200, default='')
@@ -17,7 +20,7 @@ class Computer(models.Model):
     Storage = models.CharField(max_length=200, default='')
     OS = models.CharField(max_length=200, default='')
     battery = models.CharField(max_length=200, default='')
-    warranty = models.CharField(max_length=200, default='')
+    warranty = models.CharField(max_length=3, default='')
     weight = models.CharField(max_length=200, default='')
     price = models.FloatField(default=0.0)
     description = models.CharField(max_length=200, default='')
@@ -29,6 +32,29 @@ class Computer(models.Model):
     def __str__(self):
         return str(self.comid )
     
+
+class Accessories(models.Model):
+    accid = models.CharField(max_length=13, default='', primary_key=True)
+    name = models.CharField(max_length=200, default='')
+    model = models.CharField(max_length=200, default='')
+    brand = models.CharField(max_length=200, default='')
+    color = models.CharField(max_length=200,default='')
+    keyswitch = models.CharField(max_length=200, default='')
+    keypresslifetime= models.CharField(max_length=200, default='')
+    numberkey = models.CharField(max_length=200, default='')
+    mediakey = models.CharField(max_length=200, default='')
+    interface  = models.CharField(max_length=200, default='')
+    weight = models.CharField(max_length=200, default='')
+    price = models.FloatField(default=0.0)
+    size = models.IntegerField(default=0)  
+    description = models.CharField(max_length=200, default='')
+    image = models.ImageField(upload_to='static/images/Accessories', default=None)
+    net = models.IntegerField(default=0)
+    warranty = models.CharField(max_length=3, default='')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return str(self.accid) 
 
 
 class Storage (models.Model):
@@ -44,37 +70,17 @@ class Storage (models.Model):
     Sequential_Read = models.CharField(max_length=200, default='')
     interface = models.CharField(max_length=200, default='')
     technology = models.CharField(max_length=200, default='')
-    size_ssd = models.CharField(max_length=200, default='')
+    size = models.CharField(max_length=200, default='')
     storage = models.CharField(max_length=200, default='')
     price = models.FloatField(default=0.0)
     description = models.CharField(max_length=200, default='')
     image = models.ImageField(upload_to='static/images/Storage', default=None)
     net = models.IntegerField(default=0)
-    warranty = models.CharField(max_length=200, default='')
+    warranty = models.CharField(max_length=3, default='')
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     
     def __str__(self):
         return str(self.storageid)
     
-class Accessories(models.Model):
-    accid = models.CharField(max_length=13, default='', primary_key=True)
-    brand = models.CharField(max_length=200, default='')
-    model = models.CharField(max_length=200, default='')
-    color = models.CharField(max_length=200,default='')
-    keyswitch = models.CharField(max_length=200, default='')
-    keypresslifetime= models.CharField(max_length=200, default='')
-    numberkey = models.CharField(max_length=200, default='')
-    mediakey = models.CharField(max_length=200, default='')
-    interface  = models.CharField(max_length=200, default='')
-    weight = models.CharField(max_length=200, default='')
-    price = models.FloatField(default=0.0)
-    size = models.IntegerField(default=0)  # Allowing empty values in this field
-    description = models.CharField(max_length=200, default='')
-    image = models.ImageField(upload_to='static/images/Accessories', default=None)
-    net = models.IntegerField(default=0)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    
-    def __str__(self):
-        return str(self.accid) 
 
     
